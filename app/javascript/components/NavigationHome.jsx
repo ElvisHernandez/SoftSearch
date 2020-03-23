ad80dbac2fc9cda69a5d74e44db5c02258fca6b3
ElvisHernandez <ElvisHernandezTheOne@gmail.com>
2020-03-23 11:14:06 -0400

    fixed the import statemnent for the navbar logo

--- app/javascript/components/NavigationHome.jsx
+++ app/javascript/components/NavigationHome.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-// import logo from 'app/assets/images/logo.png';
+import logo from '../../assets/images/logo.png'
 
 const Navigation = ({ isSignedIn }) => {
   return (
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3">
-    <h5 class="my-0 mr-md-auto font-weight-normal">kdpmfss Logo</h5>
-{/* <img src={logo} alt="logo" className="logo-img" /> */}
-    <nav id="navlinks" class="my-2 my-md-0 mr-md-4">
-      <a class="p-2 text-white" href="/employers/jobs/new">Post a Job</a>
-      <a class="p-2 text-white px-4" href="/map?location=GET_ALL">Find Jobs</a>
-      <a class="p-2 text-white" href="#">Post a Job</a>
-    </nav>
-    {isSignedIn ? 
-      (<a id="login" class="btn btn-outline-primary mr-3" href='/users/sign_out'>Logout</a>) :
-      <>
-      <a id="login" class="btn btn-outline-primary mr-3" href='/users/sign_in'>Login</a>
-      <a id="signup" class="btn btn-primary" href='/users/sign_up'>Sign up</a>
-      </>
-    }  
+    <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3">
+      <h5 className="my-0 mr-md-auto font-weight-normal">kdpmfss Logo</h5>
+      <img src={logo} alt="#" className="logo-img" />
+      <nav id="navlinks" className="my-2 my-md-0 mr-md-4">
+        <a className="p-2 text-white" href="/employers/jobs/new">Post a Job</a>
+        <a className="p-2 text-white px-4" href="/map?location=GET_ALL">Find Jobs</a>
+        <a className="p-2 text-white" href="#">Post a Job</a>
+      </nav>
+      {isSignedIn ? 
+        (<a id="login" className="btn btn-outline-primary mr-3" href='/users/sign_out'>Logout</a>) :
+        <>
+        <a id="login" className="btn btn-outline-primary mr-3" href='/users/sign_in'>Login</a>
+        <a id="signup" className="btn btn-primary" href='/users/sign_up'>Sign up</a>
+        </>
+      }  
     </div>
   );
 };
