1b56b121e003d7a7a9c9249ccb9b0dcc5f1ebf5f
ElvisHernandez <ElvisHernandezTheOne@gmail.com>
2020-03-24 00:03:02 -0400

    changed the jobapp form to only recieve the email and passed the jobid in through a hidden field

--- app/controllers/applicants/job_applications_controller.rb
+++ app/controllers/applicants/job_applications_controller.rb
@@ -2,27 +2,32 @@ class Applicants::JobApplicationsController < Applicants::UserBaseController
     before_action :is_current_user
    
     def index
+        @job_apps = JobApp.all
     end
 
     def new
         @job_app = JobApp.new
-        # @job_id = params['job_id']
-        # puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//////////////#{params}"
-
+        @job_id = params['job_id']
     end
 
     def create 
-        puts "????????????????????????????????????????????????????????????????????????????????????//////////////#{params.inspect}"
-        @job_app = JobApp.new(job_app_params)
+        user_email = job_app_params['email']
+        job_id = job_app_params['job_id']
+        user_id = User.where(email: user_email)[0].id
 
-        @job_app.save
-        redirect_to applicants_user_job_applications_path
+        @job_app = JobApp.new(user_id: user_id, job_id: job_id)
+
+        if @job_app.save
+            redirect_to applicants_user_job_applications_path
+        else
+            render 'new'
+        end
     end
 
     private
 
     def job_app_params
-        params.require(:job_app).permit(:user_id, :job_id)
+        params.require(:job_app).permit(:email, :job_id)
     end
 
     def is_current_user
