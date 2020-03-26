6726d2b9c1fb4af704d85638bfcc73b384f5631c
Iman Bashir <49904459+imanbashir@users.noreply.github.com>
2020-03-26 07:51:37 -0400

    font sizing fized

--- app/javascript/components/NavigationHome.jsx
+++ app/javascript/components/NavigationHome.jsx
@@ -4,18 +4,18 @@ import logo from '../../assets/images/logo.png'
 const Navigation = ({ isSignedIn }) => {
   return (
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3">
-      <a href="/"><img src={logo} alt="#" className="logo-img" /></a>
+      <h5 className="my-0 mr-md-auto font-weight-normal"><a href="/"><img src={logo} alt="#" className="logo-img"/></a></h5>
       <nav id="navlinks" className="my-2 my-md-0 mr-md-4">
         {isSignedIn && isSignedIn.employer &&
-          <a className="p-2 text-white" href={`/employers/admins/${isSignedIn.id}/jobs`}>Post a Job</a>
+          <a className="p-2 text-white find-jobs" href={`/employers/admins/${isSignedIn.id}/jobs`}>Post a Job</a>
           // /applicants/users/:user_id/job_applications)
         }
-        <a className="p-2 text-white px-4" href="/map?location=GET_ALL">Find Jobs</a>
+        <a className="p-2 text-white px-4 find-jobs" href="/map?location=GET_ALL">Find Jobs</a>
       </nav>
       {isSignedIn && !isSignedIn.employer &&
         <>
-          <a className="p-2 text-white" href={`/applicants/users/${isSignedIn.id}/job_applications`}>View Job apps</a>
-          <a className="p-2 text-white" href={`/applicants/users/${isSignedIn.id}/favorites`}>View Favorites</a>
+          <a className="p-2 text-white" href={`/applicants/users/${isSignedIn.id}/job_applications`}>Applications</a>
+          <a className="p-2 text-white" href={`/applicants/users/${isSignedIn.id}/favorites`}>Favorites</a>
         </>
 
       }
