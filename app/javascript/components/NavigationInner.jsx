c0b180ee2ccadc34219abe608f405d8f860e3df4
Iman Bashir <49904459+imanbashir@users.noreply.github.com>
2020-03-26 08:59:00 -0400

    fix inner dashboard page

--- app/javascript/components/NavigationInner.jsx
+++ app/javascript/components/NavigationInner.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
-// import logo from './logo.png';
+import logo from '../../assets/images/logo.png'
 
-const Navigation = () => {
+const Navigation = ({ isSignedIn }) => {
   return (
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3">
-    <h5 class="my-0 mr-md-auto font-weight-normal">kdpmfss Logo</h5>
-    <nav class="my-2 my-md-0 mr-md-3">
-      <a class="p-2 text-dark" href="#">Find Jobs</a>
-      <a class="p-2 text-dark" href="#">Post a Job</a>
-    </nav>
-    <a id="login" class="btn btn-outline-primary mr-3 px-4" href="#">Log In</a>
-    <a id="signup" class="btn btn-primary px-3" href="#">Sign up</a>
-  </div>
+    <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3">
+      <h5 className="my-0 mr-md-auto font-weight-normal"><a href="/"><img src={logo} alt="#" className="logo-img"/></a></h5>
+      <nav id="navlinks" className="my-2 my-md-0 mr-md-4">
+      <a className="p-2 text-white px-4 find-jobs" href="/map?location=GET_ALL">Find Jobs</a>
+        {isSignedIn && isSignedIn.employer &&
+          <a className="p-2 text-white find-jobs" href={`/employers/admins/${isSignedIn.id}/jobs`}>Post a Job</a>
+          // /applicants/users/:user_id/job_applications)
+        }
+      </nav>
+      {isSignedIn && !isSignedIn.employer &&
+        <>
+          <a className="p-2 text-white" href={`/applicants/users/${isSignedIn.id}/job_applications`}>Applications</a>
+          <a className="p-2 text-white" href={`/applicants/users/${isSignedIn.id}/favorites`}>Favorites</a>
+        </>
+
+      }
+      {isSignedIn ? 
+        (<a id="login" className="btn btn-outline-primary mr-3" href='/users/sign_out'>Logout</a>) :
+        <>
+        <a id="login" className="btn btn-outline-primary mr-3" href='/users/sign_in'>Login</a>
+        <a id="signup" className="btn btn-primary" href='/users/sign_up'>Sign up</a>
+        </>
+      }  
+    </div>
   );
 };
 
