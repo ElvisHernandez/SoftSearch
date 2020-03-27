9ca80d8082049c34ae73e425054baf0c86d9eac9
ElvisHernandez <ElvisHernandezTheOne@gmail.com>
2020-03-27 19:34:05 -0400

    added search bar to component

--- app/javascript/components/HomePageForm.jsx
+++ app/javascript/components/HomePageForm.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const HomePageForm = () => {
+    const [search, setSearch] = useState('')
+
+
     return(
-        <h1>Home Page Form</h1>
+        <form action="/search" method="post" >
+            <input className="form_input" type="text" value={search} onChange={e => {setSearch(e.target.value)}}/>
+            <input className='banner_input-button' type="submit" value="SEARCH JOBS" />
+        </form>
     )
 }
 
