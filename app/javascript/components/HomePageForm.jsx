0e8a849f27b3786453fecac0d07210618e16482e
ElvisHernandez <ElvisHernandezTheOne@gmail.com>
2020-03-27 20:06:52 -0400

    added skill buttons to component

--- app/javascript/components/HomePageForm.jsx
+++ app/javascript/components/HomePageForm.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from 'react'
 
 
-const HomePageForm = () => {
+const HomePageForm = ({ skills }) => {
     const [search, setSearch] = useState('')
 
 
     return(
-        <form action="/search" method="post" >
-            <input className="form_input" type="text" value={search} onChange={e => {setSearch(e.target.value)}}/>
-            <input className='banner_input-button' type="submit" value="SEARCH JOBS" />
-        </form>
+        <>
+            <form action="/search" method="post" >
+                <input className="form_input" type="text" value={search} placeholder="Set a Location" onChange={e => {setSearch(e.target.value)}}/>
+                <input className='banner_input-button' type="submit" value="SEARCH JOBS" />
+            </form>
+
+            <div className="search-item">
+                {skills.map(({ name }) => <button className="btn btn-md u-btn-outline-primary g-mr-10 g-mb-15">{name}</button>)}
+            </div>
+        </>
     )
 }
 
